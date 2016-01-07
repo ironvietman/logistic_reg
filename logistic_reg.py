d2daa3fd83ca12e184cecf6f7e4b7c3f13c4e135
Robert Pham <robqpham@gmail.com>
2016-01-07 14:02:01 -0700

    learn with logistic reg

--- logistic_reg.py
+++ logistic_reg.py
@@ -58,15 +58,45 @@ class Logistic_reg:
     def logistic_regression(self):
         if self.data is None:
             pass
-        idx = random.sample(range(0, 100), 100)
-        while idx:
-            sample_id = idx.pop()
-            print self.data[sample_id, :]
-            
+        num_epochs = 0
+        w1 = np.array((0, 0, 0))
+        eta = 0.01
 
+        # try a new epoch until the critiera is met
+        while True:
+            num_epochs += 1
+            w0 = w1
+            idx = random.sample(range(0, 100), 100)
+            # one epoch is a pass through all points randomly
+            for sample_id in idx:
+                x = self.data[sample_id, :]
+                y = self.target[0, sample_id]
+                # gradient decent for one point. N = 1
+                grad = (-y * x) / (1 + np.exp(y * np.inner(w1, x)))
+                w1 = w1 - eta * grad
+            # print np.linalg.norm(w0 - w1)
+            if np.linalg.norm(w0 - w1) < 0.01 or num_epochs == 3000:
+                break
+        return num_epochs
 
+    def calc_eout(self, N):
+        # Generate training data
+        X = np.random.uniform(-1, 1, (N, 2))
+        X = np.hstack((np.ones((N, 1)), X))
+        y = np.inner(self.func, X)
+        Eout = np.log(1 + np.exp(-y * np.inner(self.func, X)))
+        Eout = np.mean(Eout)
+        return Eout
 
-logistic = Logistic_reg()
-logistic.gen_targetfunc(100)
-logistic.display(logistic.data, logistic.target, logistic.func)
-logistic.logistic_regression()
+Eout = []
+num_epochs = []
+for run in range(0, 20):
+    print "run", run
+    logistic = Logistic_reg()
+    logistic.gen_targetfunc(100)
+    # logistic.display(logistic.data, logistic.target, logistic.func)
+    num_epochs.append(logistic.logistic_regression())
+    Eout.append(logistic.calc_eout(1000))
+
+print sum(Eout)/len(Eout)
+print sum(num_epochs)/len(num_epochs)
